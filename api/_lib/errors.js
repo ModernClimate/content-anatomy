1b13134cf7db6780cfabe9d4adafc3e11a13b15e
galiciapaola <galicia.paola@gmail.com>
2026-03-20 12:44:22 -0600

    Google sheet update

--- api/_lib/errors.js
+++ api/_lib/errors.js
@@ -1,5 +1,6 @@
 export function handleError(res, err) {
   console.error('[API Error]', err)
+  if (err.response?.data) console.error('[API Error Detail]', JSON.stringify(err.response.data, null, 2))
   const status = err.status || 500
   return res.status(status).json({
     success: false,
